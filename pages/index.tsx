fd4fd3b07f66761e4bb0ac448a41ac98f6b99003
Matthieu <matthieu974fr.pignolet@gmail.com>
2020-11-03 23:45:15 +0400

    First commit: Import the vuejs project & styles

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Warning } from '../components/Warning';
+
+export default function Home() {
+  return (
+    <div>
+      <Warning>
+        Message
+      </Warning>
+    </div>
+  );
+};
