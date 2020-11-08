3403ca0ae3cd08451e79eed9c008872842a2ea10
Matthieu <2650243-matthis974@users.noreply.gitlab.com>
2020-11-08 19:03:33 +0400

    Remove redis password & deployment values
    Signed-off-by: Matthieu <matthieu974fr.pignolet@gmail.com>

--- developers-house/website/abdera/src/Server.ts
+++ developers-house/website/abdera/src/Server.ts
@@ -18,7 +18,6 @@ const firstRedisNode: RedisOptions = {
     host: process.env["REDIS_HOST"],
     port: parseInt(process.env["REDIS_PORT"] || '6379'),
     password: process.env["REDIS_PASSWORD"],
-    username: process.env["REDIS_USER"],
 };
 
 export default class Server {
