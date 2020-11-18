3fa8ad7937e7dea8e048ab03961788d0eb03b0ff
Matthieu <matthieu974fr.pignolet@gmail.com>
2020-11-18 15:56:14 +0400

    Adding oauth logic & provider.

--- developers-house/login/sienna/pages/login.tsx
+++ developers-house/login/sienna/pages/login.tsx
@@ -1,26 +1,27 @@
+import { randomBytes } from 'crypto';
 import { ReactElement } from 'react';
 import { Button, ButtonContainer } from '../components/button';
 
 const platforms = ['Discord', 'GitHub', 'GitLab', 'Instagram', 'Google', 'Microsoft']
 
-export default function Home (): ReactElement {
-
-
+export default function Home(): ReactElement {
     return (
         <div>
             <h2>Login page</h2>
             <p>
                 Welcome to <b>Developer's House!</b> To continue, you need to login to your account or create one.
                 Our system does not accepts password / email login for security reasons and we propose
-                a variety of login providers available to you. You must login to continue to <b>{}</b>
+                a variety of login providers available to you. You must login to continue to <b>{ }</b>
             </p>
             <ButtonContainer>
                 {
                     platforms.map((name) => {
                         return (
-                            <Button key={name}>
-                                { name }
-                            </Button>
+                            <a key={name} href={`/dialog/api/auth/initialize/${randomBytes(256).toString('hex')}/${name.toLowerCase()}`}>
+                                <Button>
+                                    {name}
+                                </Button>
+                            </a>
                         )
                     })
                 }
