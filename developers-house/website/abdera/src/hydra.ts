cba425c56259458db3342d98278df97ced04342a
Matthieu <matthieu974fr.pignolet@gmail.com>
2020-11-20 07:50:07 +0400

    Add user endpoint for the test website

---
+++ developers-house/website/abdera/src/hydra.ts
@@ -0,0 +1,21 @@
+import { AdminApi } from "@oryd/hydra-client";
+import axios, { AxiosResponse } from "axios";
+
+const requester = axios.create({
+  headers: {
+    "X-Forwarded-Proto": "https",
+  },
+});
+
+export const AdminAPI = new AdminApi(
+  {},
+  process.env.HYDRA_ADMIN || "http://localhost:5005",
+  requester as any
+);
+
+export function validateHydraResponse<T>(response: AxiosResponse<T>): T {
+  if (response.status < 200 || response.status > 299) {
+    throw new Error("Failed to request hydra.");
+  }
+  return response.data;
+}
