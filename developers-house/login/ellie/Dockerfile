9137540394489c813896c261f2b5f7452609d351
Matthieu <matthieu974fr.pignolet@gmail.com>
2020-12-05 17:26:51 +0400

    Publish the first version of the service!

---
+++ developers-house/login/ellie/Dockerfile
@@ -0,0 +1,14 @@
+FROM node:14 as build
+WORKDIR /build
+COPY package.json .
+COPY yarn.lock .
+RUN yarn
+COPY . .
+RUN yarn compile
+
+FROM node:14
+WORKDIR /usr/bin/app
+COPY --from=build /build/dist .
+COPY --from=build /build/node_modules .
+COPY --from=build /build/package.json .
+RUN yarn start
---
+++ developers-house/login/ellie/src/endpoints/generation/AvatarLink.ts
@@ -0,0 +1,52 @@
+/*
+ * Downloads, resize and upload and image while removing all the exif data.
+ */
+
+import {Endpoint} from "../router";
+import fetch from 'node-fetch';
+import {PutObjectRequest} from 'aws-sdk/clients/s3';
+import sharp from "sharp";
+import {createHash} from 'crypto';
+import {Bucket, S3client} from "../S3";
+
+export const avatarLink: Endpoint = async (request, response) => {
+    const link = new URLSearchParams(request.url.split('?').splice(1).join('?'));
+    if (link.has('link')) {
+        const image = await fetch(link.get('link'));
+        if (!link.has('noUpload')) {
+            sharp(await image.buffer())
+                .resize(250, 250)
+                .png({
+                    quality: 80,
+                })
+                .toBuffer((err, buffer) => {
+                    if (err) console.log(err);
+                    const name = createHash('md5').update(buffer).digest('hex');
+                    const param: PutObjectRequest = {
+                        Bucket: Bucket,
+                        Key: 'avatars/' + name + '.png',
+                        Body: buffer,
+                        ACL: 'public-read',
+                        ContentType: 'image/png',
+                        ContentLength: buffer.length,
+                    };
+                    S3client.upload(param, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        response.write(JSON.stringify({
+                            link: result.Location,
+                        }));
+                        response.end();
+                    });
+                });
+        } else {
+            const d = sharp()
+                .resize(250, 250)
+                .toBuffer(() => {
+                });
+            d.pipe(response);
+            image.body.pipe(d);
+        }
+    }
+};
