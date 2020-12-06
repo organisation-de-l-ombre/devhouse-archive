30884035bbefb9bb4c1c06d6ca29e569837d2fa5
n1c00o <git.n1c00o@gmail.com>
2020-12-06 17:31:01 +0100

    [skip ci] Fix types

--- developers-house/login/ellie/src/endpoints/generation/AvatarLink.ts
+++ developers-house/login/ellie/src/endpoints/generation/AvatarLink.ts
@@ -2,17 +2,17 @@
  * Downloads, resize and upload and image while removing all the exif data.
  */
 
-import {Endpoint} from "../router";
-import fetch from 'node-fetch';
 import {PutObjectRequest} from 'aws-sdk/clients/s3';
-import sharp from "sharp";
 import {createHash} from 'crypto';
+import fetch from 'node-fetch';
+import sharp from "sharp";
+import {Endpoint} from "../router";
 import {Bucket, S3client} from "../S3";
 
 export const avatarLink: Endpoint = async (request, response) => {
-    const link = new URLSearchParams(request.url.split('?').splice(1).join('?'));
+    const link = new URLSearchParams((request.url as string).split('?').splice(1).join('?'));
     if (link.has('link')) {
-        const image = await fetch(link.get('link'));
+        const image = await fetch(link.get('link') as string);
         if (!link.has('noUpload')) {
             sharp(await image.buffer())
                 .resize(250, 250)
@@ -23,7 +23,7 @@ export const avatarLink: Endpoint = async (request, response) => {
                     if (err) console.log(err);
                     const name = createHash('md5').update(buffer).digest('hex');
                     const param: PutObjectRequest = {
-                        Bucket: Bucket,
+                        Bucket: Bucket as string,
                         Key: 'avatars/' + name + '.png',
                         Body: buffer,
                         ACL: 'public-read',
