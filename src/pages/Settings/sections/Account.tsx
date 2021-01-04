f72700a64925f4f11e428770c30a6fbb32b4ddbf
Matthieu <matthieu974fr.pignolet@gmail.com>
2021-01-04 16:28:34 +0400

    Add the avatar to the account page

--- src/pages/Settings/sections/Account.tsx
+++ src/pages/Settings/sections/Account.tsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { TitleBox } from "../../../components/ui/TitleBox";
 import { Button } from "../../../components/ui/Button";
 import { logoutUser } from "../../../state/modules/user/actions";
+import UserAvatarStatus from "../../../components/ui/UserAvatarStatus";
 
 const Account = (): ReactElement => {
   const dispatch = useDispatch();
@@ -16,6 +17,13 @@ const Account = (): ReactElement => {
   }, [dispatch]);
   return (
     <TitleBox>
+      <UserAvatarStatus
+        width="7rem"
+        statusColor="grey"
+        avatar={`https://s3.developershouse.xyz/${user?.avatar}`}
+        animate
+      />
+      <br />
       You are currently logged in as <b>{user?.username}</b> <br /> <br />
       <Button onClick={logout}>Logout</Button>
     </TitleBox>
