da42549025c836d60d2898fb10eb93e9e1e11cfe
Matthieu <matthieu974fr.pignolet@gmail.com>
2021-01-04 10:03:46 +0400

    Add startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-    echo 'ERROR: No target file given.'
-    exit 1
-fi
-
-echo $client_id > $1
-
-# Execute all other paramters
-exec "${@:2}"
+echo $client_id > /usr/share/nginx/html/.oauth
