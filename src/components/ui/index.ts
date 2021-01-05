726bd3c29c9af21056c50c29130e6d2d89cbc407
Kylian030903 <kylian.prevot@outlook.com>
2021-01-05 22:56:19 +0100

    Added Text component, and done projects webpage

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export * as ButtonGroup from "./ButtonGroup";
 export * as Card from "./Card";
 export * as TitleBox from "./TitleBox";
 export * as UserAvatarWithStatus from "./UserAvatarStatus";
+export * as Text from "./Text";
