0b04f85ca77645bf8d8af6e7974e45bedbb09c4c
Kylian030903 <kylian.prevot@outlook.com>
2021-01-05 20:54:51 +0100

    Began projects webpage

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,22 @@
+import { Card } from "components/ui/Card";
 import React, { ReactElement } from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+import rootStyles from "../../Root.module.scss";
+import projects from "./projects.temp.json";
 
 export default function ProjectsPage(): ReactElement {
-  return <></>;
+  return (
+    <div className={[rootStyles.container, rootStyles.flexColumn].join(" ")}>
+      {projects.map((project) => {
+        return (
+          <ScrollAnimation animateIn="fadeIn" key={project.name}>
+            <Card>
+              <h1>{project.name}</h1>
+              <p>{project.description}</p>
+            </Card>
+          </ScrollAnimation>
+        );
+      })}
+    </div>
+  );
 }
