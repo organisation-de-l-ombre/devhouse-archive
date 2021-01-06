ff90466b5054142d65da42d03feda2b2f249d587
Matthieu <matthieu974fr.pignolet@gmail.com>
2021-01-06 17:53:49 +0400

    Adding library functionality

---
+++ src/data-manager.ts
@@ -0,0 +1,56 @@
+import * as RabbitMQ from 'amqp-ts';
+
+export type Functions = {
+    checkIfDataAvailable: (userId: string) => Promise<boolean> | boolean;
+    provide: (userId: string) => Promise<Buffer> | Buffer;
+};
+
+/**
+ * @description A DataManager class that communicates with the DataManager service.
+ */
+export default class DataManager {
+    private connection?: RabbitMQ.Connection;
+    private queue?: RabbitMQ.Queue;
+    private exchange?: RabbitMQ.Exchange;
+
+    constructor(private readonly functions: Functions, private readonly serviceName: string, private readonly options: {
+        amqp: string;
+        exchange: string;
+    } = {
+        amqp: process.env.RABBITMQ_HOST as string,
+        exchange: 'takeout_request_created_topic'
+    }) {}
+
+    public async start () {
+        this.connection = new RabbitMQ.Connection(this.options.amqp);
+        this.queue = this.connection.declareQueue(this.serviceName);
+        await this.queue.activateConsumer(this.onMessage.bind(this));
+        this.exchange = this.connection.declareExchange(this.options.exchange);
+        await this.queue.bind(this.exchange);
+    }
+
+    private async onMessage(message: RabbitMQ.Message) {
+        const data: {
+            userId: string;
+            uuid: string;
+        } = JSON.parse(message.getContent());
+        // Uuid is the id of the bucket and userId is the id of the user.
+        // We check if data is available.
+        const exchange = this.connection?.declareExchange('takeout-callback', 'topic');
+        if (exchange && await this.functions.checkIfDataAvailable(data.userId)) {
+            // Status.
+            let msg = new RabbitMQ.Message(JSON.stringify({
+                uuid: data.uuid,
+                status: 'found',
+            }));
+            exchange.send(msg, 'takeout.callback.' + data.uuid);
+            // TODO: Upload to s3.
+            const returned = await this.functions.provide(data.userId);
+            msg = new RabbitMQ.Message(JSON.stringify({
+                uuid: data.uuid,
+                status: 'finished',
+            }));
+            exchange.send(msg, 'takeout.callback.' + data.uuid);
+        }
+    }
+}
