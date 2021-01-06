d6f34f78c4e2e4698a676c3dabbebd9648b598c2
Matthieu <matthieu974fr.pignolet@gmail.com>
2021-01-06 22:14:07 +0400

    Adding s3 upload

--- developers-house/login/datamanager/src/data-manager.ts
+++ developers-house/login/datamanager/src/data-manager.ts
@@ -1,9 +1,20 @@
 import * as RabbitMQ from 'amqp-ts';
+import S3 from 'aws-sdk/clients/s3';
 
 export type Functions = {
     checkIfDataAvailable: (userId: string) => Promise<boolean> | boolean;
     provide: (userId: string) => Promise<Buffer> | Buffer;
 };
+
+const S3client = new S3({
+    credentials: {
+        accessKeyId: process.env.TAKEOUT_AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.TAKEOUT_AWS_SECRET_ACCESS_KEY as string
+    },
+    endpoint: 'http://' + process.env.TAKEOUT_BUCKET_HOST + ':' + process.env.TAKEOUT_BUCKET_PORT,
+    s3ForcePathStyle: true
+});
+
 
 /**
  * @description A DataManager class that communicates with the DataManager service.
@@ -46,8 +57,13 @@ export class DataManager {
                 name: this.serviceName,
             }));
             exchange.send(msg, 'takeout.callback.' + data.uuid);
-            // TODO: Upload to s3.
             const returned = await this.functions.provide(data.user);
+            S3client.putObject({
+                Bucket: "takeouts",
+                Key: `${data.uuid}/${data.user}/${this.serviceName}.json`,
+                ContentType: "application/json",
+                Body: returned,
+            });
             msg = new RabbitMQ.Message(JSON.stringify({
                 uuid: data.uuid,
                 status: 'finished',
