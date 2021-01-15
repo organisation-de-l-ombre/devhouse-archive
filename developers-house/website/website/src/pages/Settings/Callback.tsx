5149b84af0e42eacb32485006611a7bfc5b2883d
Matthieu Pignolet <20992787+MatthieuCoder@users.noreply.github.com>
2021-01-15 14:53:22 +0400

    Switch to PKCE

--- developers-house/website/website/src/pages/Settings/Callback.tsx
+++ developers-house/website/website/src/pages/Settings/Callback.tsx
@@ -7,15 +7,12 @@ import { RequestParams } from "../../constants";
 
 const Callback = (): ReactElement => {
   useEffect(() => {
-    if (RequestParams.access_token && RequestParams.state) {
-      if (localStorage.getItem("state-oauth") === RequestParams.state) {
-        localStorage.removeItem("state-oauth");
-        const channel = new BroadcastChannel("callback");
-        channel.postMessage({
-          token: RequestParams.access_token,
-          state: RequestParams.state,
-        });
-      }
+    if (RequestParams.code && RequestParams.state) {
+      const channel = new BroadcastChannel("callback");
+      channel.postMessage({
+        code: RequestParams.code,
+        state: RequestParams.state,
+      });
     }
   });
 
