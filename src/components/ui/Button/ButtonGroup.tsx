5bd4d009d981e3478d62c79d0e7ae005c8f43ed1
Matthieu Pignolet <20992787+MatthieuCoder@users.noreply.github.com>
2021-01-15 22:10:03 +0400

    Fix type errors

--- src/components/ui/Button/ButtonGroup.tsx
+++ src/components/ui/Button/ButtonGroup.tsx
@@ -3,7 +3,7 @@ import { SimplifiedHTMLProperties } from "./Button";
 import styles from "./button.module.scss";
 
 const ButtonGroup: React.FC<
-  SimplifiedHTMLProperties<HTMLImageElement> & { full?: boolean }
+  SimplifiedHTMLProperties<HTMLDivElement> & { full?: boolean }
 > = ({ className, full, ...props }) => (
   <div
     className={[className, styles.group, full && styles.full].join(" ")}
