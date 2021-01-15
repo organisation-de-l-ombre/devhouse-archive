571c2a503cdcfa42fea49f0018af405aa6623342
Matthieu Pignolet <20992787+MatthieuCoder@users.noreply.github.com>
2021-01-15 19:42:53 +0400

    Fix the menu compilation

--- developers-house/website/website/src/components/navbar/Menu/MenuContainer.tsx
+++ developers-house/website/website/src/components/navbar/Menu/MenuContainer.tsx
@@ -6,10 +6,9 @@ import React from "react";
 import styles from "./navigation.module.scss";
 
 export const NavigationContainer: React.FC<
-  React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  > & { open: boolean }
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+    open: boolean;
+  }
 > = ({ className, open, ...props }) => (
   <nav
     className={[
