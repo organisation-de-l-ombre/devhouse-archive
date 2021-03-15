822167aecd73177c286006b98aaa4b07bf7b1108
matthieu <matthieu@developershouse.xyz>
2021-03-16 01:26:55 +0400

    fix(graphql): Change the graphql endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
 export const GRAPHQL_ENDPOINT =
-  "https://22309686-review-graphql-6mhtvp.matthieu-dev.xyz/";
+  "https://abdera-gateway.developershouse.xyz";
 
 export const GlobalGraphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT);
 
