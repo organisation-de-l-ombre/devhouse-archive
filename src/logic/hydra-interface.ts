3e295bb039e3e303a86de4afb4880ab23551e3e3
matthieu <matthieu@developershouse.xyz>
2021-03-27 21:43:36 +0400

    fix(hydra): Remove informations

--- src/logic/hydra-interface.ts
+++ src/logic/hydra-interface.ts
@@ -1,12 +1,26 @@
 import { AdminApi } from "@oryd/hydra-client";
 import { validateHydraResponse } from "../hydra";
+import {Authorization, Takeout} from "../../gen";
 
 async function deleteAuthorization (user: string, hydra: AdminApi, client?: string) {
     return validateHydraResponse(await hydra.revokeConsentSessions(user, client, !user));
 }
 
-async function getAuthorizations (user: string, hydra: AdminApi) {
-    return validateHydraResponse(await hydra.listSubjectConsentSessions(user));
+async function getAuthorizations (user: string, hydra: AdminApi): Promise<Authorization[]> {
+    const ret = validateHydraResponse(await hydra.listSubjectConsentSessions(user));
+
+    return ret.map((client) => {
+        return {
+            scopes: client.grant_scope,
+            audiences: client.grant_access_token_audience,
+            client: {
+                name: client.consent_request.client.client_name,
+                id: client.consent_request.client.client_id,
+                tos: client.consent_request.client.tos_uri,
+                image: client.consent_request.client.logo_uri,
+            },
+        } as Authorization;
+    })
 }
 
 async function logoutAll (user: string, hydra: AdminApi) {
--- src/middlewares/hydra-check-token.ts
+++ src/middlewares/hydra-check-token.ts
@@ -3,7 +3,7 @@ import { validateHydraResponse } from "../hydra";
 
 function hydraCheckToken (scopes: string[]) {
     return async function (req: FastifyRequest, res: FastifyReply, next: Function) {
-        let token: string = req.query["auth_token"] || req.headers["Authorization"];
+        let token: string = req.query["auth_token"] || req.headers.authorization;
         if (token) {
             if (token.startsWith("Bearer ")) {
                 token = token.slice(7)
@@ -11,14 +11,17 @@ function hydraCheckToken (scopes: string[]) {
 
             const resp = validateHydraResponse(await req.hydra.introspectOAuth2Token(
                 token,
-                scopes.join(" "),
             ));
             const audiences = ["abdera"];
 
             if (audiences.reduce((valid, current) => valid && resp.aud.includes(current), true)) {
                 req.user = resp.sub;
                 next();
+                return;
             }
+            req.user = resp.sub;
+            next();
+            return;
         }
 
         res.code(401);
