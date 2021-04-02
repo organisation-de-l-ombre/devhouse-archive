57b9c0f67e970b6570024b486de70f71f3e191cd
matthieu <matthieu@developershouse.xyz>
2021-04-03 01:18:43 +0400

    feat(endpoints/user): Add ssl mode argument

--- developers-house/login/scarlet/src/database/mod.rs
+++ developers-house/login/scarlet/src/database/mod.rs
@@ -10,12 +10,12 @@ pub mod webauth_key;
 pub fn establish_connection () -> PgConnection {
     dotenv().ok();
 
-    let url = format!("postgres://{}:{}@{}:{}/{}",
+    let url = format!("postgres://{}:{}@{}:{}/{}?sslmode=prefer",
         env::var("POSTGRES_USERNAME").expect("You need a POSTGRES_USERNAME."),
         env::var("POSTGRES_PASSWORD").expect("You need a POSTGRES_PASSWORD variable."),
         env::var("POSTGRES_HOST").expect("You need a POSTGRES_HOST."),
         env::var("POSTGRES_PORT").expect("You need a POSTGRES_PORT."),
-                      env::var("POSTGRES_DATABASE").expect("You need a POSTGRES_DATABASE.")
+        env::var("POSTGRES_DATABASE").expect("You need a POSTGRES_DATABASE.")
     );
 
     PgConnection::establish(&url)
