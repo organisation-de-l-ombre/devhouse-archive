0cf2838b47a175f9741a34b2c58352a87d848e30
matthieu <matthieu@developershouse.xyz>
2021-04-05 00:32:16 +0400

    fix(updates): Fix serviceworker update

--- src/Root.tsx
+++ src/Root.tsx
@@ -55,11 +55,11 @@ register({
             click: (): boolean => {
               registration.waiting?.postMessage({ type: "SKIP_WAITING" });
               if (registration.waiting?.state !== "activated") {
-                  registration.waiting?.addEventListener("statechange", (state) => {
-                      if (registration?.waiting?.state === "activated") {
-                        window.location.reload();
-                      }
-                  });
+                registration.waiting?.addEventListener("statechange", () => {
+                  if (registration?.waiting?.state === "activated") {
+                    window.location.reload();
+                  }
+                });
               }
               return true;
             },
