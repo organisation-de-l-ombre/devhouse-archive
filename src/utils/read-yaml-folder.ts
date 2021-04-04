500c7dd756625b970563815c5c4a0e7c25781dbc
matthieu <matthieu@developershouse.xyz>
2021-04-04 13:40:23 +0400

    fix(docker): Fix the yaml loader

--- src/utils/read-yaml-folder.ts
+++ src/utils/read-yaml-folder.ts
@@ -4,6 +4,6 @@ import { join } from "path";
 
 export async function readYamlFolder <T> (folder: string): Promise<T[]> {
     return readdir(folder)
-        .then((files) => Promise.all(files.map((path) => readFile(path)))
-            .then((files) => files.map((file) => parse(join(folder, file.toString())) as T)));
+        .then((files) => Promise.all(files.map((path) => readFile(join(folder, path))))
+            .then((files) => files.map((file) => parse(file.toString()) as T)));
 }
