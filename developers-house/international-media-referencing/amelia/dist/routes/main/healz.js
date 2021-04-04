d01ad5d4ad6498f0bd3c428a117b1922d54735a3
Kylian030903 <kylian.prevot@outlook.com>
2021-04-04 22:12:07 +0200

    First deploy of Amelia (testing only)

---
+++ developers-house/international-media-referencing/amelia/dist/routes/main/healz.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    handler: function (request, reply) {
+        void reply.code(200).send();
+    }
+};
