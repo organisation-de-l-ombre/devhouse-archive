76ff585257ffb3e9e236a8811d889357e493b5cf
matthieu <matthieu@developershouse.xyz>
2021-04-05 10:13:00 +0400

    fix(service-worker): Disable cache expiration plugin

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -6,7 +6,6 @@ import { precacheAndRoute } from "workbox-precaching/precacheAndRoute";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
 import { BackgroundSyncPlugin } from "workbox-background-sync";
-import { ExpirationPlugin } from "workbox-expiration";
 
 // eslint-disable-next-line no-restricted-globals,no-underscore-dangle
 precacheAndRoute((self as any).__WB_MANIFEST);
@@ -19,14 +18,6 @@ const runtimecache = ["/locales", "/fonts"];
 registerRoute(
   ({ url }) => runtimecache.filter((u) => url.pathname.startsWith(u))[0],
   new StaleWhileRevalidate({
-    plugins: [
-      bgSyncPlugin as never,
-      new ExpirationPlugin({
-        // Only cache requests for a week
-        maxAgeSeconds: 24 * 60 * 60,
-        // Only cache 10 requests.
-        maxEntries: 10,
-      }) as never,
-    ],
+    plugins: [bgSyncPlugin as never],
   })
 );
