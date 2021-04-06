db652aaaf840961126eafe8608e7247395564a56
matthieu <matthieu@developershouse.xyz>
2021-04-06 14:23:20 +0400

    feat(projects/logos): Add the logo to the projects

--- src/pages/Members/MemberDisplay.tsx
+++ src/pages/Members/MemberDisplay.tsx
@@ -1,11 +1,25 @@
-import React, { FC, DetailedHTMLProps, AllHTMLAttributes } from "react";
+import React, { AllHTMLAttributes, DetailedHTMLProps, FC } from "react";
 import UserAvatarStatus from "components/ui/UserAvatarStatus/UserAvatarStatus";
-import { StaffMember } from "@developers-house/abdera";
-import Button from "components/ui/Button/Button";
+import {
+  StaffMember,
+  StaffMemberSocialsIconEnum,
+} from "@developers-house/abdera";
+import Button, { ButtonImage } from "components/ui/Button/Button";
+import { FaGithub, FaGitlab, FaStackOverflow, GiClick } from "react-icons/all";
+import { FaDiscord } from "react-icons/fa";
+import { IconType } from "react-icons";
 import { getAvatar, statusToColor } from "../../utilities";
 import styles from "./member.module.scss";
 import Tooltip from "../../components/tooltip/Tooltip";
 import ButtonGroup from "../../components/ui/Button/ButtonGroup";
+
+const socialIcons: { icon: IconType; name: StaffMemberSocialsIconEnum }[] = [
+  { icon: FaStackOverflow, name: StaffMemberSocialsIconEnum.Stackoverflow },
+  { icon: FaDiscord, name: StaffMemberSocialsIconEnum.Discord },
+  { icon: FaGithub, name: StaffMemberSocialsIconEnum.Github },
+  { icon: FaGitlab, name: StaffMemberSocialsIconEnum.Gitlab },
+  { icon: GiClick, name: StaffMemberSocialsIconEnum.Website },
+];
 
 const MemberDisplay: FC<
   DetailedHTMLProps<AllHTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
@@ -43,7 +57,7 @@ const MemberDisplay: FC<
                   ) : (
                     member.presence.emote
                   ))}
-                ${member.presence.text}
+                {member.presence.text}
               </p>
             )}
           </div>
@@ -52,7 +66,34 @@ const MemberDisplay: FC<
       <p className={styles.text} />
 
       <ButtonGroup>
-        <Button>View profile</Button>
+        {member.socials &&
+          member.socials
+            .map((icon) => {
+              const { icon: IconComponent } = socialIcons.filter(
+                (other) => other.name === icon.icon
+              )[0] || {
+                icon: null,
+              };
+              return (
+                <a
+                  key={icon.name}
+                  referrerPolicy="no-referrer"
+                  target="_blank"
+                  href={icon.link}
+                  rel="noreferrer"
+                >
+                  <Button>
+                    {IconComponent && (
+                      <ButtonImage>
+                        <IconComponent />
+                      </ButtonImage>
+                    )}
+                    {icon.name}
+                  </Button>
+                </a>
+              );
+            })
+            .filter(Boolean)}
       </ButtonGroup>
     </div>
   );
