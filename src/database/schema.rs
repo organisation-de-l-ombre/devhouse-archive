22e505c16af71d0774efc83571628bdf37eba6b7
matthieu <matthieu@developershouse.xyz>
2021-04-06 21:23:13 +0400

    feat(login): Add new login session features.

---
+++ src/database/schema.rs
@@ -0,0 +1,38 @@
+table! {
+    links (id) {
+        id -> Uuid,
+        platform -> Varchar,
+        platform_id -> Varchar,
+        user_id -> Uuid,
+        updated_at -> Nullable<Timestamptz>,
+        created_at -> Timestamptz,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Uuid,
+        username -> Varchar,
+        private -> Bool,
+        roles -> Int4,
+        ban -> Nullable<Text>,
+        avatar -> Varchar,
+        twofa -> Bool,
+        otpkey -> Nullable<Varchar>,
+        updated_at -> Nullable<Timestamptz>,
+        created_at -> Timestamptz,
+    }
+}
+
+table! {
+    webauthn_keys (id) {
+        id -> Uuid,
+        user_id -> Uuid,
+        credential_id -> Varchar,
+        public_key -> Bytea,
+        updated_at -> Nullable<Timestamptz>,
+        created_at -> Timestamptz,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(links, users, webauthn_keys,);
