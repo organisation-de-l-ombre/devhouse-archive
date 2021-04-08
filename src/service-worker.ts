d11e4d54674f742d149603fba7460eff0e8a636a
Kylian030903 <kylian.prevot@outlook.com>
2021-04-08 18:54:07 +0200

    Blacklisted pages which needs API to work from service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,6 +1,5 @@
 /// <reference lib="es2015" />
 /// <reference lib="webworker" />
-
 import { precacheAndRoute } from "workbox-precaching/precacheAndRoute";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
@@ -11,10 +10,15 @@ declare const self: ServiceWorkerGlobalScope;
 precacheAndRoute((self as any).__WB_MANIFEST);
 
 const runtimeCache: string[] = ["/locales", "/fonts"];
+const runtimeBlacklist: string[] = ["/account", "/movies/title"];
 
 registerRoute(
-  ({ url }) => runtimeCache.filter((u) => url.pathname.startsWith(u)),
-  new StaleWhileRevalidate({})
+  ({ url }) =>
+    runtimeCache.filter((u): boolean => url.pathname.startsWith(u)) &&
+    runtimeBlacklist.filter(
+      (u: string): boolean => !url.pathname.startsWith(u)
+    ),
+  new StaleWhileRevalidate({ cacheName: "imr-offline-caching" })
 );
 
 self.addEventListener("message", (content) => {
