61f57d9cd668b67914aea26a32264b3142adca32
Matthieu Pignolet <20992787+MatthieuCoder@users.noreply.github.com>
2021-04-22 16:02:42 +0400

    first commit

---
+++ developers-house/ops/internal-documentation/docusaurus.config.js
@@ -0,0 +1,52 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+    title: 'Developer\'s House Documentation',
+    tagline: 'Internal documentaiton for Developer\`s house services.',
+    url: 'https://docs.matthieu-dev.xyz',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'developers-house', // Usually your GitHub org/user name.
+    projectName: 'internal-documentation', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'Developer\'s House',
+            logo: {
+                alt: 'My Site Logo',
+                src: 'img/logo.svg',
+            },
+            items: [{
+                type: 'doc',
+                docId: 'intro',
+                position: 'left',
+                label: 'Main documentation',
+            }, ],
+        },
+        footer: {
+            style: 'dark',
+            links: [],
+            copyright: `Copyright © ${new Date().getFullYear()} Developer's House. Built with Docusaurus.`,
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
+    ],
+};
