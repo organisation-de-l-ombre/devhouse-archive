e1ca822ad9a8ac6d712a1e7038425e44c3fe8692
Kylian030903 <kylian.prevot@outlook.com>
2021-04-26 22:08:24 +0200

    New implementation for projects, restored projects icons

--- src/utils/read-yaml-folder.ts
+++ src/utils/read-yaml-folder.ts
@@ -3,9 +3,37 @@ import { readdirSync, readFileSync } from "fs";
 // eslint-disable-next-line unicorn/import-style
 import { join } from "path";
 
-export function readYamlFolder<T>(folder: string): Promise<T[]> {
-  const files = readdirSync(folder);
-  return Promise.all(
-    files.map((path) => readFileSync(join(folder, path)))
-  ).then((files) => files.map((file) => parse(file.toString()) as T));
+export function readYamlFolder<T>(
+  type: "projects" | "members",
+  folder: string
+): T[] {
+  const files: T[] = [];
+
+  switch (type) {
+    case "projects": {
+      const directories: string[] = readdirSync(folder);
+
+      for (const directory of directories) {
+        const file = readFileSync(join(folder, directory, "project.yaml"));
+
+        files.push(parse(file.toString()));
+      }
+
+      break;
+    }
+
+    case "members": {
+      const members: string[] = readdirSync(folder);
+
+      for (const member of members) {
+        const file = readFileSync(join(folder, member));
+
+        files.push(parse(file.toString()));
+      }
+
+      break;
+    }
+  }
+
+  return files;
 }
