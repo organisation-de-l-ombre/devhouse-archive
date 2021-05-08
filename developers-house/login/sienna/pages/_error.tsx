c93fb20c37e6bfdd880917420c6b60a42c8733da
Kylian030903 <kylian.prevot@outlook.com>
2021-05-08 18:53:50 +0200

    Fixed layout navigation, added meta datas for pages title, embeds, etc...

--- developers-house/login/sienna/pages/_error.tsx
+++ developers-house/login/sienna/pages/_error.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import { NextRouter, useRouter } from "next/router";
+import Head from "next/head";
 import styles from "../styles/pages/error.module.scss";
 
 function Error(): ReactElement {
@@ -8,22 +9,27 @@ function Error(): ReactElement {
     router.query.error_message || router.query.error || router.query.message;
 
   return (
-    <div className={styles.error}>
-      <h2>Something wrong happened</h2>
-      <p>
-        There might be a problem with our system. Check the page you were before
-        and report the error to us. If you have any error below it can be
-        helpful to resolve the issue you encountered.
-      </p>
-      <p>
-        In any case, if the problem persists, you can report it to us on the{" "}
-        <a href="https://developershouse.xyz/support" target="blank">
-          Developer&#39;s House website
-        </a>
-        .
-      </p>
-      {error && <code className={styles["generic-margin-top"]}>{error}</code>}
-    </div>
+    <>
+      <Head key="error-page">
+        <title>Sienna - Error</title>
+      </Head>
+      <div className={styles.error}>
+        <h2>Something wrong happened</h2>
+        <p>
+          There might be a problem with our system. Check the page you were
+          before and report the error to us. If you have any error below it can
+          be helpful to resolve the issue you encountered.
+        </p>
+        <p>
+          In any case, if the problem persists, you can report it to us on the{" "}
+          <a href="https://developershouse.xyz/support" target="blank">
+            Developer&#39;s House website
+          </a>
+          .
+        </p>
+        {error && <code className={styles["generic-margin-top"]}>{error}</code>}
+      </div>
+    </>
   );
 }
 
--- developers-house/login/sienna/pages/login.tsx
+++ developers-house/login/sienna/pages/login.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Loader from "react-loaders";
+import Head from "next/head";
 import { Button, ButtonContainer } from "../components/button";
 import { fetchLogin, LoginFetchResponse } from "../lib/login";
 import styles from "../styles/pages/consent.module.scss";
@@ -31,25 +32,32 @@ export default function Login(): ReactElement {
   }
 
   return (
-    <div>
-      <h2>Login page</h2>
-      <br />
-      <p>
-        Welcome to <b>Developer&rsquo;s House!</b> To continue, you need to
-        login to your account or create one. Our system does not accepts
-        password / email login for security reasons and we propose a variety of
-        login providers available to you. You must login to continue to{" "}
-        <u>{loginSession.clientName}</u>
-      </p>
-      <ButtonContainer>
-        {loginSession.platforms.map((platform) => (
-          <a href={platform.url}>
-            <Button style={{ background: platform.color }} key={platform.name}>
-              {platform.name}
-            </Button>
-          </a>
-        ))}
-      </ButtonContainer>
-    </div>
+    <>
+      <Head key="login-page">
+        <title>Sienna - Login</title>
+      </Head>
+      <div>
+        <h2>Login page</h2>
+        <p>
+          Welcome to <b>Developer&rsquo;s House!</b> To continue, you need to
+          login to your account or create one. Our system does not accepts
+          password / email login for security reasons and we propose a variety
+          of login providers available to you. You must login to continue to{" "}
+          <u>{loginSession.clientName}</u>
+        </p>
+        <ButtonContainer>
+          {loginSession.platforms.map((platform) => (
+            <a href={platform.url}>
+              <Button
+                style={{ background: platform.color }}
+                key={platform.name}
+              >
+                {platform.name}
+              </Button>
+            </a>
+          ))}
+        </ButtonContainer>
+      </div>
+    </>
   );
 }
