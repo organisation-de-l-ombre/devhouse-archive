ec7d715f9ab1233466d01b8490f4647f479b5dfc
matthieu <matthieu@developershouse.xyz>
2021-05-08 18:13:59 +0400

    fix s3

--- developers-house/login/ellie/src/index.ts
+++ developers-house/login/ellie/src/index.ts
@@ -1,68 +1,15 @@
-/*
- * Entry point of the image generator.
- * ~ Ellie ~
- * A QR-Code and Captcha image generator
- * that helps generate stuff for the oauth flow.
- *
- * ~ Information ~
- * Maintainer: Matthieu
- * Developer: Matthieu
-*/
+import fastify from "fastify";
+import {avatarLink} from "./generators/AvatarLink";
 
-import * as Sentry from "@sentry/node";
-import * as http from 'http';
-import {createServer, IncomingMessage, OutgoingMessage, Server} from 'http';
-import morgan from 'morgan';
-import {routes} from "./endpoints/router";
+const server = fastify({
+    logger: {
+        level: "info"
+    },
+});
+server.setNotFoundHandler((_, rep) => {
+    rep.code(404);
+    rep.send();
+});
+server.route(avatarLink);
 
-    Sentry.init({
-        dsn: process.env["SENTRY_DSN"] || "Invalid Sentry DSN",
-        release: process.env["CI_COMMIT_SHORT_SHA"] || "No Commit Short SHA",
-        tracesSampleRate: 1.0,
-    });
-
-
-const logger = morgan('combined');
-
-/*
- * The request process that handles the routing.
- */
-const requestProcess = (request: IncomingMessage, response: OutgoingMessage) => {
-    if (response instanceof http.ServerResponse) {
-        logger(request, response, () => {
-            const url = (request.url as string).split('?')[0];
-            if (Object.keys(routes).includes(url)) {
-                try {
-                    routes[url](request, response);
-                } catch (e) {
-                    response.end();
-                }
-            } else {
-                response.end();
-            }
-        });
-    }
-};
-
-/*
- * Method to start the server.
- */
-const startServer = (): Promise<Server> => {
-    return new Promise<Server>((resolve) => {
-        const server = createServer(requestProcess);
-        server.listen(5000, '0.0.0.0', () => {
-            resolve(server);
-        });
-    });
-};
-
-/*
- * Start the Ellie server.
- */
-startServer()
-    .then(() => {
-        console.log('[~ Ellie ~] is now listening.');
-    })
-    .catch((error) => {
-        console.log('[~ Ellie ~] Failed to start. : ', error);
-		});
+server.listen(5000, "0.0.0.0");
