f64a358a518751c5a55e66bac919435dc4ef2ae8
matthieu <matthieu@developershouse.xyz>
2021-05-08 18:13:59 +0400

    fix s3

---
+++ src/generators/AvatarLink.ts
@@ -0,0 +1,48 @@
+import {PutObjectRequest} from 'aws-sdk/clients/s3';
+import {createHash} from 'crypto';
+import fetch from 'node-fetch';
+import sharp from "sharp";
+import {Bucket, S3client} from "../S3";
+import {RouteOptions} from "fastify";
+
+export const avatarLink: RouteOptions = {
+    method: "GET",
+    url: "/avatar-link",
+    schema: {
+        querystring: {
+            required: ["link"],
+            properties: {
+                link: { type: "string" },
+            }
+        }
+    },
+    async handler(req, res) {
+        const { link } = req.query as { link: string };
+        const response = await fetch(link);
+        if (response.ok) {
+            const buffer = await response.buffer();
+            const newImage = await sharp(buffer)
+                .resize(500, 500)
+                .png({ quality: 80 })
+                .toBuffer();
+            const name = createHash("md5").update(newImage).digest("hex");
+            const param: PutObjectRequest = {
+                Bucket: Bucket as string,
+                Key: 'avatars/' + name + '.png',
+                Body: buffer,
+                ACL: 'public-read',
+                ContentType: 'image/png',
+                ContentLength: buffer.length,
+            };
+            S3client.upload(param, (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.send({
+                    link: result.Bucket + '/' + result.Key,
+                    etag: result.ETag,
+                });
+            });
+        }
+    }
+};
