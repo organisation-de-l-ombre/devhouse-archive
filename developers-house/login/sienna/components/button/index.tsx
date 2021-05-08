431191bf48c495f9afd035353c873006f23a3179
Kylian030903 <kylian.prevot@outlook.com>
2021-05-08 20:10:56 +0200

    UI: fixed bugs and added ButtonLink element

--- developers-house/login/sienna/components/button/index.tsx
+++ developers-house/login/sienna/components/button/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./Button.module.scss";
 
-const event = (
-  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
-) => (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+const onButtonClick = (
+  onClick: (
+    event: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>
+  ) => void
+) => (
+  event: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>
+) => {
   const button = event.currentTarget;
   const circle = document.createElement("span");
   const diameter = Math.max(button.clientWidth, button.clientHeight);
   const radius = diameter / 2;
 
-  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.width = `${diameter}px`;
+  circle.style.height = `${diameter}px`;
   circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
   circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
   circle.classList.add(styles.ripple);
@@ -22,23 +27,38 @@ const event = (
 
   button.appendChild(circle);
 
-  if (onClick) {
-    onClick(event);
-  }
+  onClick(event);
 };
 
-export const Button: React.FC<
+const Button: React.FC<
   React.DetailedHTMLProps<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
   >
-> = (props) => {
+> = ({ onClick, ...props }) => {
   return (
     <button
       {...props}
+      type="button"
       className={styles.button}
-      onClick={event(props.onClick)}
+      onClick={onButtonClick(onClick)}
     />
   );
 };
+
+const ButtonLink: React.FC<
+  React.DetailedHTMLProps<
+    React.AllHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  >
+> = ({ onClick, children, ...props }) => {
+  return (
+    // eslint-disable-next-line
+    <a {...props} className={styles.button} onClick={onButtonClick(onClick)}>
+      {children}
+    </a>
+  );
+};
+
+export { Button, ButtonLink };
 export * from "./ButtonContainer";
