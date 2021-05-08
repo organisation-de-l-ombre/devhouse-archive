65fb79ce599e3eec7c41e92c6f6a8dabcd8ffe6b
matthieu <matthieu@developershouse.xyz>
2021-05-08 18:18:58 +0400

    fix s3

--- developers-house/login/ellie/src/index.ts
+++ developers-house/login/ellie/src/index.ts
@@ -10,6 +10,9 @@ server.setNotFoundHandler((_, rep) => {
     rep.code(404);
     rep.send();
 });
+server.get("/", (req, res) => {
+    res.code(200).send()
+});
 server.route(avatarLink);
 
 server.listen(5000, "0.0.0.0");
