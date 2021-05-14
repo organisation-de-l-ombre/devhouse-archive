9b42aad5ec17751782f3b1cef003b6cd0291526e
Matthieu <matthieu974fr.pignolet@gmail.com>
2021-05-14 14:19:52 +0000

    fix: prettier

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -8,22 +8,20 @@ export const Card: React.FC<SimplifiedHTMLProperties<HTMLDivElement>> = ({
   ...props
 }) => <div className={[className, styles.card].join(" ")} {...props} />;
 
-export const CardPadding: React.FC<
-  SimplifiedHTMLProperties<HTMLDivElement>
-> = ({ className, ...props }) => (
-  <div className={[className, styles.padding].join(" ")} {...props} />
-);
+export const CardPadding: React.FC<SimplifiedHTMLProperties<HTMLDivElement>> =
+  ({ className, ...props }) => (
+    <div className={[className, styles.padding].join(" ")} {...props} />
+  );
 
 export const CardHeader: React.FC<SimplifiedHTMLProperties<HTMLDivElement>> = ({
   className,
   ...props
 }) => <div className={[className, styles.header].join(" ")} {...props} />;
 
-export const CardSection: React.FC<
-  SimplifiedHTMLProperties<HTMLDivElement>
-> = ({ className, ...props }) => (
-  <div className={[className, styles.section].join(" ")} {...props} />
-);
+export const CardSection: React.FC<SimplifiedHTMLProperties<HTMLDivElement>> =
+  ({ className, ...props }) => (
+    <div className={[className, styles.section].join(" ")} {...props} />
+  );
 
 export const CardFlexContainer: React.FC<
   SimplifiedHTMLProperties<HTMLDivElement>
