6b84b416839c74e5ff4ee9e1adb2712bce797589
matthieu <matthieu@developershouse.xyz>
2021-05-18 19:57:19 +0400

    fix: service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -5,7 +5,6 @@
 import { precacheAndRoute } from "workbox-precaching/precacheAndRoute";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
-import { BackgroundSyncPlugin } from "workbox-background-sync";
 
 export type {};
 declare const self: ServiceWorkerGlobalScope;
@@ -13,16 +12,10 @@ declare const self: ServiceWorkerGlobalScope;
 // eslint-disable-next-line no-restricted-globals,no-underscore-dangle
 precacheAndRoute((self as any).__WB_MANIFEST);
 
-const bgSyncPlugin = new BackgroundSyncPlugin("myQueueName", {
-  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
-});
-
-const runtimecache = ["/locales", "/fonts"];
+const runtimecache = ["/locales", "/.oauth.json"];
 registerRoute(
   ({ url }) => runtimecache.filter((u) => url.pathname.startsWith(u))[0],
-  new StaleWhileRevalidate({
-    plugins: [bgSyncPlugin as never],
-  })
+  new StaleWhileRevalidate()
 );
 
 self.addEventListener("message", (content) => {
