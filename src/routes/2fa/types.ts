81e12e85597683451c690bc6fba4d47a25e571d6
matthieu <matthieu@developershouse.xyz>
2021-05-24 01:23:21 +0400

    feat: otp validation

--- src/routes/2fa/types.ts
+++ src/routes/2fa/types.ts
@@ -1,7 +1,8 @@
-import { User } from "@developers-house/scarlet";
+import { LoginWithPlatform, User } from "@developers-house/scarlet";
 
 export interface TwoFASession {
   challenge: string;
   user: User;
   webauthChallenge?: string;
+  login: LoginWithPlatform;
 }
