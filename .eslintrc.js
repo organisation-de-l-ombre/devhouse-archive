ef8b4912c8344565cbffa3cf6be9263543028239
Kylian030903 <kylian.prevot@outlook.com>
2021-06-10 13:11:35 +0200

    Began minor rewrite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,52 @@
 module.exports = {
-    extends: [
-        "airbnb-typescript",
-        "airbnb/hooks",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:jest/recommended",
-        "prettier",
-        "prettier/react",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "react-app",
+    "react-app/jest",
+  ],
+  plugins: ["react", "@typescript-eslint", "jest"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "linebreak-style": "off",
+    "import/prefer-default-export": "off",
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
+    "no-param-reassign": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "react/jsx-props-no-spreading": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
     ],
-    plugins: ["react", "@typescript-eslint", "jest"],
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-    },
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-    },
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2020,
-        sourceType: "module",
-        project: "./tsconfig.json",
-    },
-    rules: {
-        "linebreak-style": "off",
-        "import/prefer-default-export": "off",
-        "react/prop-types": "off",
-        "react/require-default-props": "off",
-        "no-param-reassign": "off",
-        "@typescript-eslint/triple-slash-reference": "off",
-        "react/jsx-props-no-spreading": "off",
-        "prettier/prettier": [
-            "error",
-            {
-                endOfLine: "auto",
-            },
-        ],
-        "global-require": "off",
-        "import/no-dynamic-require": "off",
-        "no-alert": "off",
-        "no-console": "off",
-    },
+    "global-require": "off",
+    "import/no-dynamic-require": "off",
+    "no-alert": "off",
+    "no-console": "off",
+  },
 };
--- src/components/modules/DisplayLanguageSVG/DisplayLanguageSVG.tsx
+++ src/components/modules/DisplayLanguageSVG/DisplayLanguageSVG.tsx
@@ -3,12 +3,14 @@ import React from "react";
 const DisplayLanguageSVG: React.FC<
   React.ImgHTMLAttributes<HTMLImageElement> & { lang: string }
 > = ({ alt, lang, ...props }) => {
-  const { default: image } = React.useMemo(
-    () => require(`../../../../public/pictures/locales/${lang}.svg`),
-    [lang]
+  return (
+    <img
+      src={`/pictures/locales/${lang}.svg`}
+      alt={alt}
+      draggable={false}
+      {...props}
+    />
   );
-
-  return <img src={image} alt={alt} draggable={false} {...props} />;
 };
 
 export default DisplayLanguageSVG;
