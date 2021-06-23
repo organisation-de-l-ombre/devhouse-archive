416d36bf157d5f0ff3efe0caf5f5a8921df73286
Matthieu <2650243-Matthis974@users.noreply.gitlab.com>
2021-06-23 13:41:16 +0000

    Revert "Update src/utils/apis.ts, deploy.yaml files"
    This reverts commit a05f2877ccd5e3f9ffabdfe8cb58644affb80bda

--- developers-house/login/candace/src/utils/apis.ts
+++ developers-house/login/candace/src/utils/apis.ts
@@ -10,11 +10,11 @@ export const Admin = new AdminApi(
       headers: { "X-Forwarded-Proto": "https" }
     }
   },
-  "http://localhost:1845"
+  "http://hydra-admin.hydra:4445"
 );
 
 const scarlet =
-  "http://localhost:8084";
+  "http://production.scarlet-22198115-production";
 export const LoginAPI = new LoginApi(undefined, scarlet);
 export const UserAPI = new UserApi(undefined, scarlet);
 export const WebAuthAPI = new WebauthApi(undefined, scarlet);
