27ad5c8b9059498087fcd928852ffa42597557f0
matthieu <matthieu@developershouse.xyz>
2021-06-26 13:52:23 +0400

    feat: faster loads, use internal endpoints for preload

--- src/utilities/endpoints.ts
+++ src/utilities/endpoints.ts
@@ -1,4 +1,4 @@
 export const getAbderaEndpoint = (): string =>
   typeof window === "undefined"
-    ? "http://production.abdera-22309686-production.svc.cluster.local"
+    ? "http://production.abdera-22309686-production.svc.cluster.local:5000"
     : "https://abdera-api.developershouse.xyz";
