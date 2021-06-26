a02f4cae4e85196390a393374d61bcf95211e2ec
matthieu <matthieu@developershouse.xyz>
2021-06-26 12:39:19 +0400

    fix: server loadable path

--- developers-house/website/website/razzle.config.js
+++ developers-house/website/website/razzle.config.js
@@ -26,15 +26,13 @@ module.exports = {
         // add loadable webpack plugin only
         // when we are building the client bundle
         if (opts.env.target === "web") {
-            const filename = path.resolve(__dirname, "build");
-
             // saving stats file to build folder
             // without this, stats files will go into
             // build/public folder
             config.plugins.push(
                 new LoadableWebpackPlugin({
                     outputAsset: false,
-                    writeToDisk: { filename },
+                    writeToDisk: { filename: "./build" },
                 })
             );
         }
