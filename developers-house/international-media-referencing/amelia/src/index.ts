5dd0236a74a2da114a3750ef8bd4a909df913c30
Matthieu <2650243-Matthis974@users.noreply.gitlab.com>
2021-07-11 17:43:56 +0000

    fix(server): Listen on all interfaces

--- developers-house/international-media-referencing/amelia/src/index.ts
+++ developers-house/international-media-referencing/amelia/src/index.ts
@@ -157,6 +157,7 @@ new (class Amelia {
   private listen(): void {
     this.FastifyClient.listen(
       process.env.PORT || 9000,
+      "0.0.0.0",
       (error: Error, address: string): void => {
         if (error) {
           throw error;
