1a892c9eaa60b5f443fb913d3a3b6d06c083b040
Matthieu <2650243-Matthis974@users.noreply.gitlab.com>
2021-07-20 22:34:26 +0000

    fix: endpoints

--- developers-house/login/candace/src/utils/apis.ts
+++ developers-house/login/candace/src/utils/apis.ts
@@ -10,11 +10,11 @@ export const Admin = new AdminApi(
       headers: { "X-Forwarded-Proto": "https" }
     }
   },
-  "http://hydra-admin.hydra:4445"
+  "http://hydra-1626807582-admin.hydra:4445"
 );
 
 const scarlet =
-  "http://developers-house-dev-login-group-scarlet.scarlet-22198115-production";
+  "http://production.scarlet-22198115-production";
 export const LoginAPI = new LoginApi(undefined, scarlet);
 export const UserAPI = new UserApi(undefined, scarlet);
 export const WebAuthAPI = new WebauthApi(undefined, scarlet);
